bb59104f1d4e5bc4914a2f45214bc72a63306066
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-05-18 17:39:17 +0300

    done

--- src/middlewares/Validator.js
+++ src/middlewares/Validator.js
@@ -1,40 +1,9 @@
 'use strict';
 
-const errorHandler = require('../error-handlers/500');
-
 module.exports = ( req,res,next ) =>{
-if( Number(req.query.name) || req.query.name === ''){
-    console.log( 'This is not a string' );
-    next(errorHandler);
+if(req.query.name){
+  next();
   }else{
-    console.log( 'This is a string' );
-    next();
+    next(new Error('Not a string!'));
   }
 };
-
-
-// function stringCheck(word) {
-//     return (req, res, next) => {
-//         if (typeof word !== 'string') {
-//             next(errorHandler);
-//         } else {
-//             req.query = word;
-//             next();// this will go to the other middleware
-//         }
-//     };
-// }
-
-// module.exports = stringCheck;
-
-
-
-// function square(n) {
-//     return (req, res, next) => {
-//         if (typeof n !== 'number') {
-//             next('not a number >:(');// this will through an error
-//         } else {
-//             req.number = n * n;
-//             next();// this will go to the other middleware
-//         }
-//     };
-// }
